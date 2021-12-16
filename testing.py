7b228ad6fd18c0d49a1dff9ea7266bd37ea3175e
Žygimantas Šlikas <zygimantas.slikas1999@gmail.com>
2021-12-16 20:53:19 +0200

    new navigation and lidar scaning

--- testing.py
+++ testing.py
@@ -7,24 +7,19 @@ import matplotlib.pyplot as plt
 class Robot_Controller:
     connection = 0
     robot = 0
-    max_dist = 2
-    
+    max_dist = 0.5
+    robot_rotation = 0
+    visited_points = []
+
     front_right_wheel = 0
     front_left_wheel  = 0
     back_right_wheel  = 0
     back_left_wheel   = 0 
-
     lidar = 0
-    right_rear_lidar = 0
-    right_front_lidar = 0
     lidar_position = 0
-
-    robot_rotation = 0
-
     front_sonar = 0
     left_sonar = 0
     right_sonar = 0
-
     line_sensor = 0
     
     def drive_forward(self, speed):
@@ -84,19 +79,19 @@ class Robot_Controller:
         if (distance[1] == 1):
             return distance[2]
         else:
-            return False
+            return 2
     def getDistanceLeft(self):
         distance = self.connection.simxReadProximitySensor(self.left_sonar, self.connection.simxServiceCall())
         if (distance[1] == 1):
             return distance[2]
         else:
-            return False
+            return 2
     def getDistanceRight(self):
         distance = self.connection.simxReadProximitySensor(self.right_sonar, self.connection.simxServiceCall())
         if (distance[1] == 1):
             return distance[2]
         else:
-            return False
+            return 2
 
     def getLineColor(self):
         value = self.connection.simxGetVisionSensorImage(self.line_sensor, True, self.connection.simxServiceCall())
@@ -126,7 +121,7 @@ class Robot_Controller:
             pos[0] -= lidar[0]*math.cos(lidar[1])
             pos[1] -= lidar[0]*math.sin(lidar[1])
             if (len(last_point) != 0):
-                if (self.euclidean_distance(last_point, pos) > 0.5):
+                if (self.euclidean_distance(last_point, pos) > 0.45):
                     corners.append([(pos[0] + last_point[0])/2, (pos[1] + last_point[1])/2, curren_direction])
             last_point = pos
             points[0].append(pos[0])
@@ -136,45 +131,96 @@ class Robot_Controller:
     def euclidean_distance(self, point1, point2):
         return math.sqrt((point1[0] - point2[0])**2+(point1[1] - point2[1])**2)
 
-    def go_to_point(self, point):
-        walls = [False,False,False]
-        target_rotation = self.get_robot_rotation() + point[2]
-        while(self.euclidean_distance(self.get_robot_position(), point[0:2]) > 0.5):    
+    def go_to_point_2(self, point):
+        front_dist = self.getDistanceFront()
+        left_dist = self.getDistanceLeft()
+        right_dist = self.getDistanceRight()
+        position = self.get_robot_position()
+        while self.euclidean_distance(position, point[0:2]) > 0.3:
+            position = self.get_robot_position()
+            self.visited_points.append(position)
+            front_dist = self.getDistanceFront()
+            left_dist = self.getDistanceLeft()
+            right_dist = self.getDistanceRight()
+            while ((self.euclidean_distance(position, point[0:2]) > 0.3) and
+            (front_dist > 0.5)):
+                position = self.get_robot_position()
+                self.visited_points.append(position)
+                front_dist = self.getDistanceFront()
+                left_dist = self.getDistanceLeft()
+                right_dist = self.getDistanceRight()
+                if left_dist < 0.4:
+                    self.turn_right(0.5)
+                    time.sleep(0.5)
+                    self.drive_forward(0)
+                if right_dist < 0.4:
+                    self.turn_left(0.5)
+                    time.sleep(0.5)
+                    self.drive_forward(0)
+                if front_dist > 0.5:
+                    self.drive_forward(1)
+                    time.sleep(0.5)
+                    self.drive_forward(0)
+                if ((front_dist>0.8)and(left_dist>0.8)) or ((front_dist>0.8)and(right_dist>0.8)) or ((left_dist>0.8)and(right_dist>0.8)):
+                    break
+            if self.euclidean_distance(position, point[0:2]) < 0.3:
+                break
+            if (front_dist <= 0.5) or ((front_dist>0.8)and(left_dist>0.8)) or ((front_dist>0.8)and(right_dist>0.8)) or ((left_dist>0.8)and(right_dist>0.8)):
+                self.rotate(self.get_relative_point_rotation(point[0:2]))
+
+    def is_point_unvisited(self, point):
+        for i in self.visited_points:
+            if self.euclidean_distance(i, point[0:2]) < 0.8:
+                return False
+        return True
+
+    def rotate(self, rotation):
+        target_rotation = self.get_robot_rotation() + rotation
+        if (abs(target_rotation)//math.pi > 0):
+            if (target_rotation > 0):
+                target_rotation = (-1*math.pi) + (target_rotation % math.pi)
+            else: 
+                target_rotation = math.pi - (abs(target_rotation) % math.pi) 
+        if (rotation > 0):
             while not(0.3>(target_rotation - self.get_robot_rotation())>-0.3):
-                var = self.get_robot_rotation()
                 self.turn_left(1)
                 time.sleep(0.3)
                 self.drive_forward(0)
-            walls[0] = self.getDistanceLeft()
-            walls[1] = self.getDistanceFront()
-            walls[2] = self.getDistanceRight()
-            for i in range(0, len(walls)):
-                if (walls[i] != False and walls[i] < self.max_dist):
-                    walls[i] = True
-                else:
-                    walls[i] = False
-            if (walls[1] == False):
-                self.drive_forward(1)
-                time.sleep(0.5)
+        elif (rotation < 0):
+            while not(0.3>(target_rotation - self.get_robot_rotation())>-0.3):
+                self.turn_right(1)
+                time.sleep(0.3)
                 self.drive_forward(0)
-            elif (walls[2] == False):
-                while (self.getDistanceFront() != False):
-                    self.turn_right(1)
-                    time.sleep(0.5)
-                    self.drive_forward(0)
-                self.drive_forward(1)
-                time.sleep(1)
-                self.drive_forward(0) 
-            elif (walls[0] == False):
-                while (self.getDistanceFront() != False):
-                    self.turn_left(1)
-                    time.sleep(0.5)
-                    self.drive_forward(0)
-                self.drive_forward(1)
-                time.sleep(1)
-                self.drive_forward(0)                
 
-
+    def get_relative_point_rotation(self, point):
+        robot_point = self.get_robot_position()
+        robot_rotation = self.get_robot_rotation()
+        robot_to_point_distance = self.euclidean_distance(point, robot_point)
+        vector_to_point = [0,0]
+        vector_to_point[0] = point[0]-robot_point[0]
+        vector_to_point[1] = point[1]-robot_point[1]
+        points_absolute_rotation = math.acos(-1*vector_to_point[0]/(robot_to_point_distance))
+        if (self.euclidean_distance(point, [robot_point[0] - math.cos(points_absolute_rotation)*robot_to_point_distance, robot_point[1] - math.sin(points_absolute_rotation)*robot_to_point_distance]) 
+        < self.euclidean_distance(point, [robot_point[0] - math.cos(points_absolute_rotation)*robot_to_point_distance, robot_point[1] + math.sin(points_absolute_rotation)*robot_to_point_distance])):
+            return points_absolute_rotation - robot_rotation
+        else:
+            return -1*(points_absolute_rotation) - robot_rotation
+   
+def zip_points(points, old_bit_map):
+    points2 = [[],[]]
+    points2[0] = [int((i//0.1)+30) for i in points[0]]
+    points2[1] = [int((i//0.1)+30) for i in points[1]]
+    for i in range(0, len(points2[0])):
+            old_bit_map[points2[1][i], points2[0][i]] = 1
+    return old_bit_map
+def get_points_from_bit_map(bit_map):
+    points = [[],[]]
+    for i in range(0, 61):
+        for j in range(0, 61):
+            if (bit_map[j,i] == 1):
+                points[0].append(((i-30)/10)+0.05)
+                points[1].append(((j-30)/10)+0.05)
+    return points
 if __name__ == "__main__":
     with b0RemoteApi.RemoteApiClient('Robot_Python_Controller','b0RemoteApi',60) as client:    
         robot1 = Robot_Controller() 
@@ -186,21 +232,15 @@ if __name__ == "__main__":
         robot1.back_left_wheel = client.simxGetObjectHandle('back_left_motor',client.simxServiceCall())[1]
         robot1.lidar = client.simxGetObjectHandle('lidar_ray',client.simxServiceCall())[1]
         robot1.lidar_position = client.simxGetObjectHandle('lidar_pos',client.simxServiceCall())[1]
-
         robot1.front_sonar = client.simxGetObjectHandle('front_proximity_sensor',client.simxServiceCall())[1]
         robot1.left_sonar = client.simxGetObjectHandle('left_proximity_sensor',client.simxServiceCall())[1]
         robot1.right_sonar = client.simxGetObjectHandle('right_proximity_sensor',client.simxServiceCall())[1]
-
-        robot1.right_rear_lidar = client.simxGetObjectHandle('right_rear_lidar',client.simxServiceCall())[1]
-        robot1.right_front_lidar = client.simxGetObjectHandle('right_front_lidar',client.simxServiceCall())[1]
-
         robot1.line_sensor = client.simxGetObjectHandle('Vision_sensor',client.simxServiceCall())[1]
 
         robot1.drive_forward(0)
-        points = [[],[]]
+        zeros = np.zeros((61,61))
 
         while True:
-            #scan with lidar
             data = robot1.lidar_scan_around()
             plt.axis([-3,3,-3,3])
             plt.plot(data[0][0], data[0][1], 'o')
@@ -209,102 +249,19 @@ if __name__ == "__main__":
             plt.plot(robot_position[0], robot_position[1], '*r')
             plt.show()
             print(data[1])
-            points[0].append(data[0][0])
-            points[1].append(data[0][1])
+            zeros = zip_points(data[0], zeros)
+            points = get_points_from_bit_map(zeros)
+            plt.axis([-3,3,-3,3])
             plt.plot(points[0], points[1], 'o')
             plt.plot(robot_position[0], robot_position[1], '*r')
             plt.show()
-            robot1.go_to_point(data[1][0])
-            print("end")
-            # if state == 1:
-            #     robot1.followWall(max_dist)
 
-            #     distFront = robot1.getDistanceFront(max_dist)
-            #     distRight = robot1.getDistanceRight(max_dist)
-
-            #     color = robot1.getLineColor()
-
-            #     print(distRight)
-
-            #     if distFront == 1:
-            #         state = 2
-
-            #     if distRight == 0:
-            #         state = 3
-
-            #     # TODO: if vision sensor detect black color - stop simulation
-            #     # if color == "black"
-            #     #     state = 4
-            # elif state == 2:
-            #     print("wall ahead, turning left . . .")
-            #     robot1.turn_left(0.5)
-            #     time.sleep(0.5)
-            #     state = 1
-            # elif state == 3:
-            #     print("no wall, lets turn here right  . . .")
-            #     robot1.turn_right(0.9)
-            #     time.sleep(0.9)
-            #     robot1.drive_forward(0.5)
-            #     time.sleep(0.5)
-            #     state = 1
-            # # elif state == 4:
-            #     # TODO: stop simulation, goal reached
-                
-
-        # while True:
-        #     right_rear_lidar = robot1.get_right_rear_data()
-        #     right_front_lidar = robot1.get_right_front_data()
-
-            
-        #     distFront = robot1.getDistanceFront(max_dist)
-        #     distLeft = robot1.getDistanceLeft(max_dist)
-        #     distRight = robot1.getDistanceRight(max_dist)
-
-
-        
-
-            #     if right_rear_lidar == right_front_lidar:
-            #         print("driving forward")
-            #         robot1.drive_forward(0.5)
-            #         time.sleep(0.5)
-            #     elif right_front_lidar > right_rear_lidar:
-            #         print("need to adjust to right")
-            #         robot1.turn_right(0.5)
-            #         time.sleep(0.5)
-            #     elif right_front_lidar < right_rear_lidar:
-            #         print("need to adjust to left")
-            #         robot1.turn_left(0.5)
-            #         time.sleep(0.5)
-
-        #     robot1.drive_forward(0.5)
-
-
-        #     if distFront == max_dist:
-        #         print("Wall ahead !")
-
-        #         if distLeft == max_dist:
-        #             print("have to turn right !")
-        #             robot1.turn_right(0.5)
-        #             time.sleep(0.5)
-        #         elif distRight == max_dist:
-        #             print("have to turn left !")
-        #             robot1.turn_left(0.5)
-        #             time.sleep(0.5)
-        #         else:
-        #             print("need to turn somwhere")
-        #             robot1.drive_backward(0.5)
-        #             time.sleep(1)
-        #             robot1.turn_left(0.5)
-        #             time.sleep(1)
-        
-            # plt.axis([-3,3,-3,3])
-            # plt.scatter(points[0], points[1])
-
-            # for i in range(0, 50):
-            #     point = robot1.get_lidar_point()
-            #     points[0].append(point[0])
-            #     points[1].append(point[1])
-            #     plt.scatter(point[0], point[1])
-            #     plt.pause(0.001)
-
+            unvisited = [x for x in data[1] if robot1.is_point_unvisited(x)]
+            if (len(unvisited) > 0):
+                print("goint to " + str(unvisited[0]))
+                robot1.go_to_point_2(unvisited[0])
+            else:
+                print("everything visited")
+                break
+            print("next iteration")
         client.simxCloseScene(client.simxServiceCall())
